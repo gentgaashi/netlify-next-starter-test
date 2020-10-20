75be00e7b46a3e08a3027661c6cb22be61d15dd4
Gent Gashi <gentgaashi@gmail.com>
2020-10-20 20:37:56 +0200

    added styled-components 2

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,26 +1,26 @@
+import styled from 'styled-components';
+
+const Sfooter = styled.footer`
+  width: 100%;
+  height: 100px;
+  border-top: 1px solid #eaeaea;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+const Simg = styled.footer`
+  height: 1em;
+  margin: 5px;
+`;
+
 export default function Footer() {
   return (
     <>
-      <footer>
+      <Sfooter>
         Made with{' '}
-        <img src="/netliheart.svg" alt="Netlify Logo" className="logo" /> for
+        <Simg src="/netliheart.svg" alt="Netlify Logo" /> for
         you
-      </footer>
-      <style jsx>{`
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        .logo {
-          height: 1em;
-          margin: 5px;
-        }
-      `}</style>
+      </Sfooter>
     </>
   )
 }
